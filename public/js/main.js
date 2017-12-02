e3303de42ace204d869ead64db7ff4f6a0baab11
decent <danielekwuazi@gmail.com>
2017-12-02 21:36:06 +0700

    function to clear input field

--- public/js/main.js
+++ public/js/main.js
@@ -11,21 +11,32 @@ socket.on('smsStatus', (data) => {
     status.innerHTML = `<h5>Text message has be sent to ${data.number}</h5>`; 
 })
 
+function clear() {
+    numberInput.value = '';
+    textInput.value = '';
+}
+
 function send () {
     const number = numberInput.value.replace(/\D/g, ''),
           text = textInput.value;
-
-    fetch('/', {
-        method: 'post',
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify({number: number, text: text})
-    })
-    .then((res)=> {
-        console.log(res);
-    })
-    .catch((err)=> {
-        console.log(err);
-    });
+    
+    if(number !== '' && text !==''){
+        fetch('/', {
+            method: 'post',
+            headers: {
+              'Content-type': 'application/json'
+            },
+            body: JSON.stringify({number: number, text: text})
+        })
+        .then((res)=> {
+            console.log(res);
+        })
+        .catch((err)=> {
+            console.log(err);
+        });
+    }else{
+        console.log('Input field is empty');
+    }
+    
+    clear();
 }
