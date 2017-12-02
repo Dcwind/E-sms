48b73ef2c7cce4dbee09e0ec765bd1abe71bc098
decent <danielekwuazi@gmail.com>
2017-12-02 11:46:49 +0700

    post method

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,25 @@
-const number = document.getElementById('number'),
-      text = document.getElementById('text'),
+const numberInput = document.getElementById('number'),
+      textInput = document.getElementById('text'),
       button = document.getElementById('button'),
       status = document.querySelector('.response');
 
 button.addEventListener('click', send, false);
 
+function send () {
+    const number = numberInput.nodeValue.replace(/\D/g, ''),
+          text = textInput.value;
+
+    fetch('/', {
+        method: 'post',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({number: number, text: text})
+    })
+    .then((res)=> {
+        console.log(res);
+    })
+    .catch((err)=> {
+        console.log(err);
+    })
+}
