871e2a5cebaaddd9dd675c8304360bea6d450ccd
decent <danielekwuazi@gmail.com>
2017-12-02 09:54:09 +0700

    get data from client side

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+const number = document.getElementById('number'),
+      text = document.getElementById('text'),
+      button = document.getElementById('button'),
+      status = document.querySelector('.response');
+
+button.addEventListener('click', send, false);
+
